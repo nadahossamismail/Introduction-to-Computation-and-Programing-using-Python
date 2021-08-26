090e9dc7d5fa3a10788711d1078212de60967cd2
nadahossamismail <89613015+nadahossamismail@users.noreply.github.com>
2021-08-27 00:59:27 +0200

    Add files via upload

---
+++ Problem Sets/Pset0/pset0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 22:26:30 2021
+
+@author: th
+"""
+
+import math
+x= int(input("Enter x:"))
+y=int(input("Enter y:"))
+print("x**y=",x**y)
+print("log x to the base of 2=",math.log(x,2))
